86c5e17f1671ffba0c8113879f6b975898a9d120
zonoryo <k022g1035@g.neec.ac.jp>
2022-10-14 09:39:04 +0900

    新規ブランチで文章追加してコミット

--- gittest.cpp
+++ gittest.cpp
@@ -5,7 +5,8 @@
 
 int main()
 {
-    std::cout << "Hello VisualStudio Git!\n";
+    std::cout << "Hello GitHub Would\n";
+    std::cout << "Hello New brauch\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
